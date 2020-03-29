8f6e9b4a441a5a9c28a1a4aa35b207f2e33f7396
wentong <wentong@meituan.com>
2020-03-29 16:48:02 +0800

    修改返回

--- src/main/java/com/jike/certification/controller/RoleJurisdictionRelController.java
+++ src/main/java/com/jike/certification/controller/RoleJurisdictionRelController.java
@@ -1,9 +1,11 @@
 package com.jike.certification.controller;
 
+import com.jike.certification.model.Response;
 import com.jike.certification.model.roleJurisdictionRelevance.RoleJurisdictionRelevanceInsertReq;
 import com.jike.certification.model.roleJurisdictionRelevance.RoleJurisdictionRelevanceVo;
 import com.jike.certification.model.thirdRole.RoleJurisdictionVo;
 import com.jike.certification.service.RoleJurisdictionRelevanceService;
+import com.jike.certification.util.ResponseUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,8 +25,8 @@ public class RoleJurisdictionRelController {
 
     @PostMapping("addRelevance")
     @ApiOperation("新增角色权限关联")
-    public RoleJurisdictionRelevanceVo addRelevance(@RequestBody RoleJurisdictionRelevanceInsertReq insertReq){
-        return relevanceService.getOrAddRel(insertReq);
+    public Response<RoleJurisdictionRelevanceVo> addRelevance(@RequestBody RoleJurisdictionRelevanceInsertReq insertReq){
+        return ResponseUtil.makeSuccess(relevanceService.getOrAddRel(insertReq));
     }
 
     @GetMapping("deleteRelevance")
