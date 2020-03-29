346ffe2b7290396f22c17112965b7d589faf4982
wentong <wentong@meituan.com>
2020-03-29 13:57:08 +0800

    addCorsMappings:添加请求头

--- src/main/java/com/jike/certification/config/MvcConfigurerAdapter.java
+++ src/main/java/com/jike/certification/config/MvcConfigurerAdapter.java
@@ -5,9 +5,15 @@ import com.jike.certification.interceptor.TokenValidateInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -57,6 +63,26 @@ public class MvcConfigurerAdapter implements WebMvcConfigurer {
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             //放行哪些原始域(头部信息)
             .allowedHeaders("*");
+    }
+
+    private CorsConfiguration addcorsConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        List<String> list = new ArrayList<>();
+        list.add("*");
+        corsConfiguration.setAllowedOrigins(list);
+    /*
+    // 请求常用的三种配置，*代表允许所有，当时你也可以自定义属性（比如header只能带什么，只能是post方式等等）
+    */
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", addcorsConfig());
+        return new CorsFilter(source);
     }
 }
 
